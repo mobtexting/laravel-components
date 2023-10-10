6d75bcb0c84f82a2aaf31efd508d617d667f75f8
Suda Sankara Rao <sankar@mobtexting.com>
2023-10-10 07:41:45 +0000

    fix: space issue between filters

--- resources/views/bootstrap-4/card/filter.blade.php
+++ resources/views/bootstrap-4/card/filter.blade.php
@@ -1,3 +1,3 @@
-<div {!! $attributes->merge(['class' => 'mt-2 mr-2 d-flex justify-content-end page-header']) !!}>
+<div {!! $attributes->merge(['class' => 'mt-2 mr-2 d-flex justify-content-end page-header card-filters']) !!}>
     {!! $slot !!}
 </div>
